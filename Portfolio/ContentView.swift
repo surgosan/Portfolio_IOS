823fb4f06bde51087ccfb0ebffc6b85224fe90c2
surgosan <surgosan@gmail.com>
2024-06-23 17:11:11 -0400

    Changed name area | Added language "Hello World"s

--- Portfolio/ContentView.swift
+++ Portfolio/ContentView.swift
@@ -7,15 +7,26 @@ extension Color {
 }
 
 struct ContentView: View {
+    @State private var currentLanguageIndex: Int = 0
     @Environment(\.colorScheme) var colorScheme // Setting the users setting as an environment variable
 
     var dynamicTheme: Color { // Checks if colorScheme == dark. If it is, return orange, else return blue
         colorScheme == .dark ? Color(red: 214/255, green: 90/255, blue: 49/255) : Color(red: 93/255, green: 139/255, blue: 244/255)
     }
+    
+    let texts = [
+            ("System.out.println(\"Hello World\");", "Java"),
+            ("printf(\"Hello, World!\");", "C"),
+            ("<h1> Hello World </h1>", "HTML"),
+            ("--text: \"Hello World\"", "CSS"),
+            ("console.log(\"Hello World\");", "JavaScript"),
+            ("print(\"Hello World\")", "Swift"),
+        ]
+    
 
     var body: some View {
         ZStack { // Use Z Stack to set the background of the app
-            if colorScheme == .dark { // Check if colorSheme is dark or light and use appropriate color
+            if colorScheme == .dark { // Check if colorSheme is dark or light and use appropriate
                 Color.black.edgesIgnoringSafeArea(.all)
             } else {
                 Color.white.edgesIgnoringSafeArea(.all)
@@ -24,31 +35,47 @@ struct ContentView: View {
             VStack {
                     HStack {
                         Image("portrait") // Insert portrait image
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 110, height: 110)
-                            .clipShape(Circle())
-                            .overlay(Circle().stroke(dynamicTheme, lineWidth: 1))
-                            .shadow(radius: 10)
-                        
-                        HStack {
-                            Text("S")
-                                .font(.system(size: 80, weight: .bold, design: .default))
-                                .foregroundColor(dynamicTheme)
-                                
-                            
+                            .resizable() // Allow to be resized
+                            .aspectRatio(contentMode: .fill) // = background-size: fill
+                            .frame(width: 80, height: 80) // Size of the object
+                            .clipShape(Circle()) // Cut into a circle
+                            .overlay(Circle().stroke(dynamicTheme, lineWidth: 1)) // Border
+                            .shadow(radius: 10) // Shadow
+
                             VStack(alignment: .leading) {
-                                Text("ergio")
+                                Text("Sergio")
                                     .font(.title) // Name
                                     .multilineTextAlignment(.leading)
                                 
-                                Text("anchez-Alvarez") // Name
+                                Text("Sanchez-Alvarez") // Name
                                     .font(.title2)
+                                    .foregroundColor(Color.gray)
                                     .multilineTextAlignment(.leading)
                             }
-                        }
                     }
                 Text("Student | Musician | Programmer")
+                
+                VStack { // Display "Hello World" in different languages
+                    Text(texts[currentLanguageIndex].0) // Hello World
+                        .font(.callout)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                    
+                    HStack { // Language Container
+                        Spacer()
+                        Text(texts[currentLanguageIndex].1)
+                            .font(.caption)
+                            .fontWeight(.thin)
+                            .multilineTextAlignment(.trailing)
+                    }
+                }
+                .padding(.all, 10.0)
+                .background(Color.gray.opacity(0.5))
+                .cornerRadius(5.0)
+                .onAppear { // Create the timer to iterate over languages
+                    Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { timer in
+                        currentLanguageIndex = (currentLanguageIndex + 1) % texts.count
+                    }
+                }
             }
             .padding()
         }
